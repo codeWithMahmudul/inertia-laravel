3eaded5d94b086eef704192b36d1438fc044bc01
MdMahmudulHasanGit <mahmudul209307@gmail.com>
2024-02-08 12:06:55 +0600

    Dashboard App

---
+++ resources/js/Pages/Contact.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <h2>Hello to Contact</h2>
+    <Link href="/">Home</Link><br />
+    <Link href="/contact">Contact</Link><br />
+    <Link href="/about">About</Link>
+  </div>
+</template>
+  
+  <script setup>
+import { Link } from "@inertiajs/vue3";
+</script>
+  
+  <style lang="scss" scoped>
+</style>
---
+++ resources/js/Pages/Welcome.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <AppLayout>
+      <header class="bg-white shadow">
+        <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+          <h1 class="text-3xl font-bold tracking-tight text-gray-900">Hello</h1>
+        </div>
+      </header>
+      <main>
+        <div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+          <!-- Your content -->
+        </div>
+      </main>
+    </AppLayout>
+  </div>
+</template>
+  
+  <script setup>
+import { Link } from "@inertiajs/vue3";
+import AppLayout from "../Layouts/App.vue";
+</script>
+
+<style lang="scss" scoped>
+</style>
